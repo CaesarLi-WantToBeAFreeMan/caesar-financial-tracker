ebc8bd9e5e1998820f925640338c51355e834eac
CaesarLi-WantToBeAFreeMan <krbw0110010000@gmail.com>
2025-12-16 21:54:18 +0800

    initialize backend directory

---
+++ backend/src/main/java/com/caesarjlee/caesarfinancialtracker/CaesarfinancialtrackerApplication.java
@@ -0,0 +1,11 @@
+package com.caesarjlee.caesarfinancialtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesarfinancialtrackerApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(CaesarfinancialtrackerApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/caesarjlee/caesarfinancialtracker/CaesarfinancialtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caesarjlee.caesarfinancialtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesarfinancialtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
