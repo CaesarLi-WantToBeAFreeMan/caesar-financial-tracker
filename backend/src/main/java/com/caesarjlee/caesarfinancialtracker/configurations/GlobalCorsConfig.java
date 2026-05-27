f45e86287353b1cb4e85db637117fe98b9ff0bd6
Caesar James LEE <krbw0110010000@gmail.com>
2026-05-27 12:48:56 +0800

    fix cors configuration

--- backend/src/main/java/com/caesarjlee/caesarfinancialtracker/configurations/GlobalCorsConfig.java
+++ backend/src/main/java/com/caesarjlee/caesarfinancialtracker/configurations/GlobalCorsConfig.java
@@ -10,26 +10,24 @@ import java.util.*;
  * allowed origins:
  * local: http://localhost:5173
  * cloud: https://caesaris.net
-*/
+ */
 @Configuration
 public class GlobalCorsConfig {
-    @Value("${app.cors.allowed-origins}")
-    private String allowedOriginsRaw;
+    @Value("${app.cors.allowed-origins:*}") private String allowedOriginsRaw;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        List<String> origins = Arrays.stream(allowedOriginsRaw.split(","))
-                                    .map(String::trim)
-                                    .filter(s -> !s.isEmpty())
-                                    .toList();
+        List<String> origins =
+            Arrays.stream(allowedOriginsRaw.split(",")).map(String::trim).filter(s -> !s.isEmpty()).toList();
 
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(origins);
+        // corsConfiguration.setAllowedOrigins(origins);
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setExposedHeaders(List.of("Authorization", "Content-Disposition"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setMaxAge(3600L);//cache for 1 hour
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setMaxAge(3_600L);   // cache for 1 hour
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
